3fb52c2e7959b92a709cb6ebfe81a6b186a5193e
Diogo Correia <xavi@Xavi-pc.lan>
2026-03-26 15:44:47 +0000

    Don Goon, big Daddy - 1989, Tohn Torque, John Goon

--- AI_connect4.c
+++ AI_connect4.c
@@ -6,6 +6,12 @@
 #define MAX_Y 6
 #define MAX_X 7
 
+void initiate_board(char board[MAX_X][MAX_Y]);
+void print_board(char board[MAX_X][MAX_Y]);
+int player_input(char board[MAX_X][MAX_Y]);
+bool check_insert(char board[MAX_X][MAX_Y], int pos);
+void insert(char board[MAX_X][MAX_Y], char player, int pos);
+
 
 //creates/resets the board state
 void initiate_board(char board[MAX_X][MAX_Y])
@@ -30,6 +36,7 @@ void print_board(char board[MAX_X][MAX_Y])
         printf("|\n");
     }
     for(int i=0;i<MAX_X+2;i++) {printf("-"); }
+    printf("\n\n");
 }
 
 
@@ -37,26 +44,31 @@ void print_board(char board[MAX_X][MAX_Y])
 int player_input(char board[MAX_X][MAX_Y])
 {
     int input;
-    do
+    while(true)
     {
         scanf("%d",&input);
-        if(input<0 || input >8) {printf("the number must be between 1-7"); }
-    } while (input<0 || input >8);
-    return input;
+        if(input<-1 || input>7) {printf("the number must be between 1-7\n"); }
+        else if(!check_insert(board, input-1)) {printf("chose somewhere thats not occupied\n"); }
+        else {break; }
+    }
+    return input-1;
 }
 
 
 //check if you can insert in pos
-bool checks_insert(char board[MAX_X][MAX_Y], int pos) {return board[pos][0]!='_'; }
+bool check_insert(char board[MAX_X][MAX_Y], int pos) {return board[pos][0]=='_'; }
 
 
 //allows players to insert their respective piece in the board
 void insert(char board[MAX_X][MAX_Y], char player, int pos)
 {
     int y=0;
-    while(board[pos][y+1] == '_' || y == MAX_Y) {y++; }
-    board[y][pos] = player;
+    while(board[pos][y] == '_' && y<6) {y++; }
+    if(y!=0) {board[pos][y-1] = player; }
+    else {board[pos][y] = player; }
 }
+
+//win condition
 
 
 
@@ -65,9 +77,14 @@ int main()
     char game[MAX_X][MAX_Y];
     initiate_board(game);
     print_board(game);
+    int player;
     while(true)
     {
-        insert(game, '#', 2);
+        player = player_input(game);
+        insert(game,'#',player);
+        print_board(game);
+        player = player_input(game);
+        insert(game,'*',player);
         print_board(game);
     }
 
