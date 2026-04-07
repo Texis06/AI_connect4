a01754e9587eff5cf6d3372768a40207ff6baca3
Diogo Correia <xavi@Xavi-pc.lan>
2026-04-07 18:39:03 +0100

    tudo menos procurar bugs e colocar empates

--- AI_connect4.c
+++ AI_connect4.c
@@ -5,12 +5,12 @@
 /*
 TODO:
 -diferentes empates(campo cheio = POSSIBILIDADE de empate // se o gamestate for repetido 3 vezes então há POSSIBILIDADE de empate);
--win condition de insert;
+-win condition de insert; X
 -win condition de pop(jogador que faz o pop tem a prioridade na vitória);
 -colocar as funções feitas no topo;
--pop;
+-pop; X
 -seleção jogadas;
--alterar board para ser global;
+-alterar board para ser global; X
 */
 
 
@@ -21,6 +21,9 @@ TODO:
 bool true_board[MAX_X][MAX_Y];
 char board[MAX_X][MAX_Y];
 bool victory=false;
+bool board_filled=false;
+
+
 
 void initiate_board();
 void print_board();
@@ -33,8 +36,12 @@ int vertical_win_con(int x, int y, char player);
 int diagonal_win_con(int x, int y, char player, int dir);
 void reset_true_board();
 bool check_top_insert(int i);
-void option_selector();
 bool check_pop(int pos);
+void pop(int pos, char player);
+void print_ruleset();
+void option_selector(char player);
+char pop_win_con(int posx, int posy, char player);
+
 
 
 //creates/resets the board state
@@ -76,7 +83,7 @@ void print_board()
 }
 
 
-
+/*
 //allows player to choose which coordinate to play
 int player_input_insert()
 {
@@ -90,6 +97,8 @@ int player_input_insert()
     }
     return input-1;
 }
+*/
+
 
 //checks if its possible to insert, basicamente se o topo está todo cheio
 bool check_top_insert(int i)
@@ -99,15 +108,57 @@ bool check_top_insert(int i)
     else {return check_top_insert(i++); }
 } 
 
-//selects the option you want to choose
-void option_selector()
+void print_ruleset()
 {
-    int option;
-    printf("choose if you want to pop out (1) or insert (2), write number of your option:");
-    scanf("%d", &option);
+    printf("\n\n==========================================================================================");
+    printf("\nrules for PopOut:\n");
+    printf("-if you wish the choose a different position after selecting, choose 0 for the option;\n");
+    printf("-if there is only one option available choose any number besides 0 to confirm the play\n");
+    printf("-if the board is completely filled with pieces, you will have an option to tie the game;\n");
+    printf("\n==========================================================================================\n\n");
 }
 
-
+//selects the option you want to choose
+void option_selector(char player)
+{
+    int pos=0, option=0;
+    while(pos==0)
+    {
+        printf("player %c, choose which position(1-7) you want to target:", player);
+        while(true)
+        {
+            scanf("%d", &pos);
+            if(pos<7 && pos>0) {break; }
+            else {printf("\nThe position must be between 1-7"); }
+        }
+        if(check_insert(pos-1) && !check_pop(pos-1))
+        {
+            printf("choose if you want to insert(1) or pop(2):");
+            scanf("%d", &option);
+            if(option==1) {insert(player,pos); }
+            else if(option==2) {pop(pos, player); }
+        }
+        else if(check_insert(pos-1))
+        {
+            printf("you can only insert:");
+            scanf("%d", &option);
+            if(option!=0) {insert(player, pos); }
+        }
+        else
+        {
+            printf("you can only pop:");
+            scanf("%d", &option);
+            if(option!=0) {pop(pos, player); }
+        }
+    }
+}
+/*    while(true)
+    {
+        scanf("%d",&input);
+        if(input<-1 || input>7) {printf("the number must be between 1-7\n"); }
+        else if(!check_insert(input-1)) {printf("chose somewhere thats not occupied\n"); }
+        else {break; }
+    }*/
 
 
 //check if you can insert in pos
@@ -120,10 +171,10 @@ bool check_pop(int pos) {return board[pos][MAX_Y-1]=='_'; }
 void insert(char player, int pos)
 {
     int y=0;
-    while(board[pos][y] == '_' && y<6) {y++; }
-    if(y!=0) {board[pos][y-1] = player; }
-    else {board[pos][y] = player; }
-    if(insert_win_con(player, pos, y-1))
+    while(board[pos-1][y] == '_' && y<6) {y++; }
+    if(y!=0) {board[pos-1][y-1] = player; }
+    else {board[pos-1][y] = player; }
+    if(insert_win_con(player, pos-1, y-1))
     {
         printf("\n================");
         printf("\nplayer %c victory\n", player);
@@ -132,6 +183,24 @@ void insert(char player, int pos)
     }
 }
 
+char pop_win_con(int posx, int posy, char player)
+{
+    if(horizontal_win_con(posx, posy, player) >= 4) {return player; }
+    reset_true_board();
+    if(diagonal_win_con(posx, posy, player, -1) >= 4) {return player; }
+    reset_true_board();
+    if(diagonal_win_con(posx, posy, player, 1) >= 4) {return player; }
+    reset_true_board();
+    if(player=='#') {player='*'; }
+    else {player='#'; }
+    if(horizontal_win_con(posx, posy, player) >= 4) {return player; }
+    reset_true_board();
+    if(diagonal_win_con(posx, posy, player, -1) >= 4) {return player; }
+    reset_true_board();
+    if(diagonal_win_con(posx, posy, player, 1) >= 4) {return player; }
+    reset_true_board();
+    return '_';
+}
 
 //win condition
 bool insert_win_con(char player, int posx, int posy)
@@ -171,21 +240,25 @@ int diagonal_win_con(int x, int y, char player, int dir)
     return 1 + diagonal_win_con(x+1, y-dir, player, dir) + diagonal_win_con(x-1, y+dir, player, dir);
 }
 
-
-
-
 //pop
-void pop(int pos)
+void pop(int pos,char player)
 {
-    board[pos][MAX_Y-1] = '_';
+    board[pos-1][MAX_Y-1] = '_';
     int i = MAX_Y-1;
     while(true)
     {
         if(i-1 < 0 || board[MAX_X][i-1] == '_') {break; }
-        board[pos][i] = board[pos][i-1];
+        board[pos-1][i] = board[pos-1][i-1];
+        if(!victory && pop_win_con(pos-1, i, player) != '_')
+        {
+            printf("\n================");
+            printf("\nplayer %c victory\n", pop_win_con(pos-1, i, player));
+            printf("================\n");
+            victory = true;
+        }
         i--;
     }
-    board[pos][i] = '_';
+    board[pos-1][i] = '_';
 }
 
 
@@ -193,22 +266,19 @@ void pop(int pos)
 int main()
 {
     initiate_board();
+    print_ruleset();
     print_board();
     reset_true_board();
-    int pos;
+    //int pos;
     char player='#';
     while(!victory)
     {
-        pos = player_input_insert();
-        insert(player,pos);
+        //pos = player_input_insert();
+        option_selector(player);
         print_board();
         if(player == '#') {player = '*'; }
         else {player = '#'; }
     }
-    scanf("%d", &pos);
-    pop(pos-1);
-    print_board();
-
 
     return 0;
 }
